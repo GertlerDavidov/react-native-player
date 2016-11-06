43b9a720798abb0214af5771a6ae41fae6f4c959
rammuk <ram.mukmel@gmail.com>
2016-11-06 21:29:19 +0100

    Smoother duration

--- android/player/src/main/java/com/xeodou/rctplayer/ReactAudio.java
+++ android/player/src/main/java/com/xeodou/rctplayer/ReactAudio.java
@@ -298,7 +298,7 @@ public class ReactAudio extends ReactContextBaseJavaModule implements ExoPlayer.
 
                 sendEvent("onUpdatePosition", params);
             }
-        }, 500);
+        }, 100);
     }
 
     /**
